715b420e8c10540b46508561454b856a0a5d67c0
RebeccaHough <rebecca.c.hough@blueyonder.co.uk>
2019-04-23 08:47:35 +0100

    fix: test http methods

--- src/app/components/tests/tests.component.ts
+++ src/app/components/tests/tests.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpService } from '../../services/http.service';
+import { Message } from 'src/app/interfaces/message.interface';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-tests',
@@ -8,19 +10,33 @@ import { HttpService } from '../../services/http.service';
 })
 export class TestsComponent {
 
-  constructor(private http: HttpService) { }
+  constructor(private http: HttpService, private messageService: MessageService) { }
 
   /**
    * Force back-end to generate a report and send it to the email address saved
    */
   public sendReport() {
-    this.http.sendReport();
+    this.http.sendReport().subscribe((res: Message) => {
+      res.message = 'Successfuly generated report.\n' + res.message;
+      this.messageService.setMessage(res);
+    },
+    (err: Message) => {
+      err.message = 'Failed to generate report.\n' + err.message;
+      this.messageService.setMessage(err);
+    });;
   }
 
   /**
    * Send a test email to the email address saved
    */
   public sendTestEmail() {
-    this.http.sendTestEmail();
+    this.http.sendTestEmail().subscribe((res: Message) => {
+      res.message = 'Successfuly sent email.\n' + res.message;
+      this.messageService.setMessage(res);
+    },
+    (err: Message) => {
+      err.message = 'Failed to send email.\n' + err.message;
+      this.messageService.setMessage(err);
+    });;
   }
 }
